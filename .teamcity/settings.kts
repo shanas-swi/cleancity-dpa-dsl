86097c0461b33056e6096cc23864d5dc4cf244c4
safraz.hanas <safraz.hanas@TeamCity>
2022-08-09 00:23:52 -0500

    TeamCity change in 'DPA / DPA - CleanCity / DPA Installers' project: project was created from URL

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -28,6 +29,8 @@ version = "2021.2"
 project {
 
     buildType(Build)
+
+    subProject(DpaInstallers)
 }
 
 object Build : BuildType({
@@ -40,5 +43,38 @@ object Build : BuildType({
     triggers {
         vcs {
         }
+    }
+})
+
+
+object DpaInstallers : Project({
+    name = "DPA Installers"
+
+    vcsRoot(DpaInstallers_HttpsGithubComSolarwindsDpaGitRefsHeadsMaster)
+
+    buildType(DpaInstallers_Build)
+})
+
+object DpaInstallers_Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(DpaInstallers_HttpsGithubComSolarwindsDpaGitRefsHeadsMaster)
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+})
+
+object DpaInstallers_HttpsGithubComSolarwindsDpaGitRefsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/solarwinds/dpa.git#refs/heads/master"
+    url = "https://github.com/solarwinds/dpa.git"
+    branch = "refs/heads/master"
+    branchSpec = "refs/heads/*"
+    authMethod = password {
+        userName = "shanas-swi"
+        password = "credentialsJSON:bf61d350-be02-4443-bc11-aea55db613a2"
     }
 })
